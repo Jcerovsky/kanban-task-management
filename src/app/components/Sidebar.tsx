4ced920a1bfc74cb89010f1553ebf50c3832a877
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-09-13 17:29:20 +0930

    add show sidebar functionality and transition properties

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -38,8 +38,8 @@ function Sidebar() {
           {boardList?.map((board, index) => (
             <div
               key={board}
-              className={`flex gap-2 items-center p-3 mr-4 rounded-r-full cursor-pointer hover:text-violet-500 
-              hover:bg-violet-100 hover:text-violet-500 delay-100 transition-transform ${
+              className={`flex gap-2 items-center p-3 mr-4 rounded-r-full cursor-pointer 
+              hover:bg-violet-100 hover:text-violet-500 transition-all delay-100 ease-in-out ${
                 selectedBoardIndex === index
                   ? "text-white bg-violet-500 hover:text-violet-500 delay-100"
                   : ""
@@ -51,8 +51,8 @@ function Sidebar() {
             </div>
           ))}
           <div
-            className="flex gap-2 items-center p-3 mr-4 rounded-r-full cursor-pointer hover:text-violet-500
-              hover:bg-violet-100 text-violet-500 delay-100 transition-transform"
+            className="flex gap-2 items-center p-3 mr-4 rounded-r-full cursor-pointer
+              hover:bg-violet-100 text-violet-500 transition-all delay-100 ease-in-out"
             onClick={() =>
               setIsShown((prevState) => {
                 return {
@@ -66,7 +66,10 @@ function Sidebar() {
           </div>
         </div>
 
-        <div className="flex mt-auto items-center gap-2 justify-around bg-neutral-200 dark:bg-black py-3 px-4 m-5 rounded-md">
+        <div
+          className="flex mt-auto items-center gap-2 justify-around bg-neutral-200 dark:bg-black py-3 px-4 m-5
+        rounded-md transition-all delay-100 ease-in-out"
+        >
           <img src="../../../assets/icon-light-theme.svg" alt="sun icon" />
 
           <label className="relative inline-flex items-center cursor-pointer">
@@ -79,7 +82,7 @@ function Sidebar() {
             <div
               className="w-11 h-6 rounded-full peer-checked:after:translate-x-full
           peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white
-          after:border after:rounded-full after:h-5 after:w-5 after:transition-all bg-violet-600 outline-none"
+          after:border after:rounded-full after:h-5 after:w-5 after:transition-all delay-300 ease-in-out bg-violet-600 outline-none"
               onClick={() => setTheme(theme === "light" ? "dark" : "light")}
             ></div>
             <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300" />
@@ -92,14 +95,26 @@ function Sidebar() {
         </div>
         <div
           className="flex gap-2 justify-center items-center p-3 mr-4 rounded-r-full cursor-pointer hover:text-violet-500
-              hover:bg-violet-100 hover:text-violet-500 delay-100 transition-transform"
+              hover:bg-violet-100 hover:text-violet-500 transition-all delay-100 ease-in-out"
         >
-          <img src="../../../assets/icon-hide-sidebar.svg" alt="" />
+          <img src="../../../assets/icon-hide-sidebar.svg" alt="crossed eye" />
           <p onClick={() => setIsSideBarHidden(true)}>Hide Sidebar</p>
         </div>
         {isShown["new-board"] && <AddNewBoard />}
       </div>
-      <div className="absolute"></div>
+      {isSidebarHidden && (
+        <div
+          className="absolute flex justify-center bottom-10 items-center p-5  rounded-r-full cursor-pointer
+          hover:bg-violet-400 bg-violet-500 w-14 hover:w-24 transition-all delay-100 ease-in-out"
+          onClick={() => setIsSideBarHidden(false)}
+        >
+          <img
+            src="../../../assets/icon-show-sidebar.svg"
+            alt="opened eye"
+            className="w-5"
+          />
+        </div>
+      )}
     </>
   );
 }
