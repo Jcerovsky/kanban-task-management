679491e87fae57f70195828cd7b661fb0b7d22ad
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-09-14 07:16:32 +0930

    finish styling delete board

--- src/app/components/DeleteBoard.tsx
+++ src/app/components/DeleteBoard.tsx
@@ -1,30 +1,32 @@
 import React, { useContext, useState } from "react";
 import { Context } from "@/app/context/Context";
-import Button from "@/app/components/Button";
 
 function DeleteBoard() {
-  const { currentBoard, setIsShown } = useContext(Context)!;
+  const { currentBoard } = useContext(Context)!;
   const [isHidden, setIsHidden] = useState<boolean>(false);
 
   return (
     <div
-      className={`delete-board bg-white rounded-md absolute font-bold top-20 right-20 flex flex-col gap-3 w-1/2 p-6 ${
-        isHidden ? "hidden" : ""
-      }`}
+      className={`delete-board bg-white dark:bg-slate-800 rounded-md absolute font-semibold top-60 right-20 flex flex-col gap-6 w-1/2 p-6 
+      shadow-xl ${isHidden ? "hidden" : ""}`}
     >
       <h3 className="text-xl text-red-600">Delete this board?</h3>
       <p className="text-slate-500 text-sm">
         {`Are you sure you want to delete the ${currentBoard} board? This action
               will remove all columns and tasks and cannot be reversed.`}
       </p>
-      <div className="flex space-between">
+      <div className="flex justify-between ">
         <button
-          className={"bg-red-500 w-1/2  py-2 text-white bg-hover rounded-full"}
+          className={
+            "bg-red-500 hover:bg-red-400 py-2 w-[48%] text-white bg-hover rounded-full transition-all ease-in-out duration-300"
+          }
         >
           Delete
         </button>
         <button
-          className={"bg-neutral-200 w-1/2 text-violet-400 py-2 rounded-full"}
+          className={
+            "bg-neutral-200 hover:bg-neutral-300 w-[48%] text-violet-400 py-2 rounded-full transition-all ease-in-out duration-300"
+          }
           onClick={() => setIsHidden(true)}
         >
           Cancel
