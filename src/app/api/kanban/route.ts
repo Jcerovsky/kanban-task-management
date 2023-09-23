ca00a4586e24f91e969afb3f86e2804023cf9a94
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-09-23 21:49:29 +0930

    cors fix

--- src/app/api/kanban/route.ts
+++ src/app/api/kanban/route.ts
@@ -1,6 +1,13 @@
 import boards from "./data.json";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  return NextResponse.json(boards.boards);
+export async function GET(request: Request) {
+  return NextResponse.json(boards.boards, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 }
